aeee75690e82aad18910ad308e08ebd0e89fa6eb
Anders Wallin <anders.e.e.wallin@gmail.com>
2018-05-24 11:08:20 +0300

    print out what example code is doing

--- example.py
+++ example.py
@@ -11,15 +11,22 @@ def main():
 
     # Example:
     nr = 2**22
+    print("Generating WPM, nr=%d" % nr)
     # generate white phase noise (WPM)
     X1 = cn.noiseGen(nr, 2.0e-11**2*1/3, 0)
+    
+    print("Generating WFM, nr=%d" % nr)
     # generate white frequency noise (WFM)
     X2 = cn.noiseGen(nr, 3.5e-13**2, -2)
+    
+    print("Generating FF, nr=%d" % nr)
     # generate flicker floor noise (FF)
     X3 = cn.noiseGen(nr, 1.0e-16**2*(np.pi/(2*np.log(2))), -3)
 
+    print("Writing data to file.")
     with open("noise.txt", 'w') as f:
         f.writelines( "%s\n" % item for item in X1+X2+X3)
+    print("Done.")
 
 if __name__=="__main__":
     main()
